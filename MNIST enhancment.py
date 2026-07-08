73f2bb1853292523bb4035f23272985ee7c8bb3c
Suhay2011 <Suhayl.abrahams3@gmail.com>
2026-07-08 13:17:04 +0200

    Create MNIST enhancment.py

---
+++ MNIST enhancment.py
@@ -0,0 +1,10 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+datagen = ImageDataGenerator(
+    rotation_range=10,
+    zoom_range=0.1,
+    width_shift_range=0.1,
+    height_shift_range=0.1
+)
+
+datagen.fit(x_train)
